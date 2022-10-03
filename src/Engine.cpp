f3b564064ffc62306cd209a41dba41b6842b4677
vadimszinatulins <vadimz2@hotmail.com>
2022-10-03 21:09:17 +0100

    Added base glad

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -4,9 +4,13 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_video.h>
 #include <SimpleTaskManager/Scheduler.h>
+#include <glad/glad.h>
 
 SDL_Window *m_window { nullptr };
 SDL_GLContext m_context { nullptr };
+
+const auto WIDHT { 640 };
+const auto HEIGHT { 480 };
 
 namespace be {
 
@@ -27,8 +31,13 @@ void Engine::init() {
 
 	SDL_GL_SetSwapInterval(1);
 
-	m_window = SDL_CreateWindow("BitEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	m_window = SDL_CreateWindow("BitEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDHT, HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 	m_context = SDL_GL_CreateContext(m_window);
+
+	gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+
+	glViewport(0, 0, WIDHT, HEIGHT);
+	glClearColor(0.23f, 0.65f, 0.15f, 1.0f);
 
 	// Initialize SimpleTaskManager
 	stm::Scheduler::getInstance().initialize();
@@ -48,6 +57,8 @@ void Engine::loop() {
 				break;
 			}
 		}
+
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		SDL_GL_SwapWindow(m_window);
 	}
