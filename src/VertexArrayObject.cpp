af10090e383c52a9876f3e1eb8315d5d64174cb6
vadimszinatulins <vadimz2@hotmail.com>
2022-10-04 17:15:01 +0100

    Added Vertex Array Object

---
+++ src/VertexArrayObject.cpp
@@ -0,0 +1,138 @@
+#include "VertexArrayObject.h"
+
+#include <glad/glad.h>
+
+namespace be::graphics {
+
+VertexArrayObject VertexArrayObject::create() {
+	VertexArrayObject vao;
+
+	vao.init();
+
+	return vao;
+}
+
+void VertexArrayObject::init() {
+	if(!m_id) {
+		glGenVertexArrays(1, &m_id);
+	}
+}
+
+void VertexArrayObject::clear() {
+	if(m_id) {
+		if(m_ebo) {
+			glDeleteBuffers(1, &m_ebo);
+			m_ebo = 0;
+		}
+
+		glDeleteBuffers(m_buffers.size(), m_buffers.data());
+		
+		glDeleteVertexArrays(1, &m_id);
+		m_id = 0;
+
+		m_vertexCount = 0;
+	}
+}
+
+void VertexArrayObject::enable() {
+	if(!m_id) {
+		return;
+	}
+	glEnableVertexAttribArray(m_id);
+}
+
+void VertexArrayObject::disable() {
+	glEnableVertexAttribArray(0);
+}
+
+void VertexArrayObject::draw() {
+	if(m_ebo) {
+		glDrawElements(GL_TRIANGLES, m_vertexCount, GL_UNSIGNED_INT, 0);
+	} else {
+		glDrawArrays(GL_TRIANGLES, 0, m_vertexCount / 3);
+	}
+}
+
+void VertexArrayObject::addAttribute(std::vector<char> data, int componentsPerVertex) {
+	addAttribute(data.data(), data.size(), componentsPerVertex, GL_BYTE, sizeof(char));
+}
+
+void VertexArrayObject::addAttribute(std::vector<unsigned char> data, int componentsPerVertex) {
+	addAttribute(data.data(), data.size(), componentsPerVertex, GL_UNSIGNED_BYTE, sizeof(unsigned char));
+}
+
+void VertexArrayObject::addAttribute(std::vector<short> data, int componentsPerVertex) {
+	addAttribute(data.data(), data.size(), componentsPerVertex, GL_SHORT, sizeof(short));
+}
+
+void VertexArrayObject::addAttribute(std::vector<unsigned short> data, int componentsPerVertex) {
+	addAttribute(data.data(), data.size(), componentsPerVertex, GL_UNSIGNED_SHORT, sizeof(unsigned short));
+}
+
+void VertexArrayObject::addAttribute(std::vector<int> data, int componentsPerVertex) {
+	addAttribute(data.data(), data.size(), componentsPerVertex, GL_INT, sizeof(int));
+}
+
+void VertexArrayObject::addAttribute(std::vector<unsigned int> data, int componentsPerVertex) {
+	addAttribute(data.data(), data.size(), componentsPerVertex, GL_UNSIGNED_INT, sizeof(unsigned int));
+}
+
+void VertexArrayObject::addAttribute(std::vector<float> data, int componentsPerVertex) {
+	addAttribute(data.data(), data.size(), componentsPerVertex, GL_FLOAT, sizeof(float));
+}
+
+void VertexArrayObject::addAttribute(void *data, long dataSize, int componentsPerVertex, int type, int typeSize) {
+	// Make sure the Vertex Arary Object has been initialized before setting attributes
+	if(!m_id) {
+		return;
+	}
+
+	// Get the number of vertices in the attribute
+	unsigned int vertexCount = dataSize / (componentsPerVertex * typeSize);
+
+	if(!m_vertexCount) {
+		// If the number of vertices hasn't been set then set it
+		m_vertexCount = vertexCount;
+	} else if(vertexCount != m_vertexCount) {
+		// If the number of vertices is different from what has been sent then the data is invalid
+		return;
+	}
+
+	glBindVertexArray(m_id);
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, dataSize, data, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(m_buffers.size(), componentsPerVertex, type, GL_FALSE, typeSize, (void*)0);
+	glEnableVertexAttribArray(m_buffers.size());
+
+	m_buffers.push_back(buffer);
+
+	glBindVertexArray(0);
+}
+
+void VertexArrayObject::setElementBuffer(std::vector<unsigned int> data) {
+	// Make sure the Vertex Arary Object has been initialized before setting attributes
+	if(!m_id) {
+		return;
+	}
+
+	// Make sure that Element Buffer Object hasn't been set yet
+	if(m_ebo) {
+		return;
+	}
+
+	glBindVertexArray(m_id);
+
+	glGenBuffers(1, &m_ebo);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, data.size(), data.data(), GL_STATIC_DRAW);
+
+	glBindVertexArray(m_id);
+}
+
+}
---
+++ src/include/BitEngine/graphics/VertexArrayObject.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+
+namespace be::graphics {
+
+class VertexArrayObject {
+public:
+	VertexArrayObject() = default;
+	~VertexArrayObject() = default;
+
+	static VertexArrayObject create();
+
+	void init();
+	void clear();
+
+	void enable();
+	void disable();
+
+	void draw();
+
+	void addAttribute(std::vector<char> data, int componentsPerVertex);
+	void addAttribute(std::vector<unsigned char> data, int componentsPerVertex);
+	void addAttribute(std::vector<short> data, int componentsPerVertex);
+	void addAttribute(std::vector<unsigned short> data, int componentsPerVertex);
+	void addAttribute(std::vector<int> data, int componentsPerVertex);
+	void addAttribute(std::vector<unsigned int> data, int componentsPerVertex);
+	void addAttribute(std::vector<float> data, int componentsPerVertex);
+
+	void setElementBuffer(std::vector<unsigned int> data);
+private:
+	void addAttribute(void *data, long dataSize, int componentsPerVertex, int type, int typeSize);
+
+	std::vector<unsigned int> m_buffers {};
+	unsigned int m_id { 0 };
+	unsigned int m_ebo { 0 };
+	unsigned int m_vertexCount { 0 };
+};
+
+}
